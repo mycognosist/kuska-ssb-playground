9617e8b4f036e443657cdaef79001d9b6e92683b
mycognosist <gnomad@cryptolab.net>
2021-01-31 14:08:35 +0000

    Add Patchwork key example for load and write

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # kuska ssb playground
 
 Basics examples and documentation for the [kuska-ssb](https://github.com/Kuska-ssb/ssb) library (a Secure Scuttlebutt library written in Rust).
+
+## Notes
+
+The `examples/generate_keys.rs` example returns a compiler error if compiled with a `nightly` toolchain. Use the `default` compiler toolchain instead. 
--- examples/generate_keys.rs
+++ examples/generate_keys.rs
@@ -1,17 +1,43 @@
+use async_std::fs::File;
+
+use kuska_ssb::keystore;
 use kuska_ssb::keystore::OwnedIdentity;
 
-fn main() {
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[async_std::main]
+async fn main() -> Result<()> {
+
+    // struct OwnedIdentity {
+    //     id: String,
+    //     pk: ed25519::PublicKey,
+    //     sk: ed25519::SecretKey,
+    // }
 
     // create an OwnedIdentity struct with id, public key and private key
     let identity = OwnedIdentity::create();
 
-    /* struct OwnedIdentity {
-     *     id: String,
-     *     pk: ed25519::PublicKey,
-     *     sk: ed25519::SecretKey,
-     * }
-     */
-    
     // print the identity
     println!("{:#?}", identity);
+
+    // load an identity from file (Patchwork interoperability)
+    let patchwork_identity = keystore::from_patchwork_local().await.expect("read local secret");
+
+    // print the patchwork identity from file
+    println!("{:#?}", patchwork_identity);
+
+    // create test file for storing patchwork identity
+    let mut file = File::create("patchwork_id").await?;
+
+    // write patchwork identity to file
+    keystore::write_patchwork_config(&identity, &mut file).await.expect("write patchwork secret");
+
+    // {
+    //   "id": "@z+GWOeqa8SKcAwdVe79UFI0ayoTGs+ZsAM+NwuTve4A=.ed25519",
+    //   "curve": "ed25519",
+    //   "public": "z+GWOeqa8SKcAwdVe79UFI0ayoTGs+ZsAM+NwuTve4A=.ed25519",
+    //   "private": "vzgGRxbqmPLR3Lx4eiwyJXitfeYQdmaMrkZfx0J1UbP4ZY56prxIpwDB1V7v1QUjRrKhMaz5mwAz43C5O97gA==.ed25519"
+    // }
+
+    Ok(())
 }
