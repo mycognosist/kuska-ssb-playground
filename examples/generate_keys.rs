d8f21031eb0a8fdc9dbf77e4e4f00bab459348c8
mycognosist <gnomad@cryptolab.net>
2021-01-31 11:47:27 +0000

    Add struct definition as comment for OwnedIdentity

--- examples/generate_keys.rs
+++ examples/generate_keys.rs
@@ -1,9 +1,17 @@
 use kuska_ssb::keystore::OwnedIdentity;
 
 fn main() {
-    // create an OwnedIdentity with public and private keys
+
+    // create an OwnedIdentity struct with id, public key and private key
     let identity = OwnedIdentity::create();
 
+    /* struct OwnedIdentity {
+     *     id: String,
+     *     pk: ed25519::PublicKey,
+     *     sk: ed25519::SecretKey,
+     * }
+     */
+    
     // print the identity
     println!("{:#?}", identity);
 }
